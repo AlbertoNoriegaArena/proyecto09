912130f543a17105f112bbe379f7ee9d6417c487
curso06 <curso06@gmail.com>
2024-11-25 14:18:32 +0100

    Amotaciones

--- src/main/java/es/santander/ascender/proyecto09/controller/SolicitudController.java
+++ src/main/java/es/santander/ascender/proyecto09/controller/SolicitudController.java
@@ -2,26 +2,41 @@ package es.santander.ascender.proyecto09.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import es.santander.ascender.proyecto09.Solicitud;
 
+@RestController
+@RequestMapping("solicitud")
 public class SolicitudController {
-
+    
+    @PostMapping
     public long crear(){
         throw new UnsupportedOperationException("Pendiente");
     }
 
-    public Solicitud leer ( long id){
+    @GetMapping("/{id}")
+    public Solicitud leer (@PathVariable("id") long id){
         throw new UnsupportedOperationException("Pendiente");
     }
 
+    @GetMapping
     public List <Solicitud> listar(){
         throw new UnsupportedOperationException("Pendiente");
     }
 
+    @PutMapping
     public void modificar(Solicitud solicitud){
         throw new UnsupportedOperationException("Pendiente");
     }
 
+    @DeleteMapping
     public void eliminar(long id){
         throw new UnsupportedOperationException("Pendiente");
     }
