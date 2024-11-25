c83528c64b02e1f1e12b02b3c15033591bfff4d0
curso06 <curso06@gmail.com>
2024-11-25 14:01:32 +0100

    Ejemplo html y css y primera clase y controlador

---
+++ src/main/java/es/santander/ascender/proyecto09/controller/SolicitudController.java
@@ -0,0 +1,28 @@
+package es.santander.ascender.proyecto09.controller;
+
+import java.util.List;
+
+import es.santander.ascender.proyecto09.Solicitud;
+
+public class SolicitudController {
+
+    public long crear(){
+        throw new UnsupportedOperationException("Pendiente");
+    }
+
+    public Solicitud leer ( long id){
+        throw new UnsupportedOperationException("Pendiente");
+    }
+
+    public List <Solicitud> listar(){
+        throw new UnsupportedOperationException("Pendiente");
+    }
+
+    public void modificar(Solicitud solicitud){
+        throw new UnsupportedOperationException("Pendiente");
+    }
+
+    public void eliminar(long id){
+        throw new UnsupportedOperationException("Pendiente");
+    }
+}
