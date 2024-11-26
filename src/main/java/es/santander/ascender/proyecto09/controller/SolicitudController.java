ec39d0619b1519a29f5c98d0f8ac20e805e3a999
curso06 <curso06@gmail.com>
2024-11-26 12:53:20 +0100

    Servidor rest

--- src/main/java/es/santander/ascender/proyecto09/controller/SolicitudController.java
+++ src/main/java/es/santander/ascender/proyecto09/controller/SolicitudController.java
@@ -1,5 +1,6 @@
 package es.santander.ascender.proyecto09.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -7,37 +8,61 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import es.santander.ascender.proyecto09.Solicitud;
 
 @RestController
-@RequestMapping("solicitud")
+@RequestMapping("/rest/solicitud")
 public class SolicitudController {
-    
+
     @PostMapping
-    public long crear(){
-        throw new UnsupportedOperationException("Pendiente");
+    // El objeto Solicitud te lo van a pasar en el body
+    public long crear(@RequestBody Solicitud solicitud) throws Exception {
+        // si no me pasa id por defecto => 0
+        if (solicitud.getId() != 0) {
+            throw new Exception("Al crear no me puedes pasar el id");
+        }
+
+        return 16;
     }
 
     @GetMapping("/{id}")
-    public Solicitud leer (@PathVariable("id") long id){
-        throw new UnsupportedOperationException("Pendiente");
+    public Solicitud leer(@PathVariable("id") long id) {
+        Solicitud respuesta = new Solicitud();
+
+        respuesta.setId(id);
+        respuesta.setNombre("Juan");
+        respuesta.setEdad(28);
+
+        return respuesta;
     }
 
     @GetMapping
-    public List <Solicitud> listar(){
-        throw new UnsupportedOperationException("Pendiente");
+    public List<Solicitud> listar() {
+        List<Solicitud> resultado = new ArrayList<>();
+        for (int i = 3; i <= 15; i += 3) {
+            Solicitud respuesta = new Solicitud();
+
+            respuesta.setId(i);
+            respuesta.setNombre("Juan " + i);
+            respuesta.setEdad(28 + i);
+            resultado.add(respuesta);
+        }
+        return resultado;
     }
 
     @PutMapping
-    public void modificar(Solicitud solicitud){
-        throw new UnsupportedOperationException("Pendiente");
+    public void modificar(@RequestBody Solicitud solicitud) throws Exception {
+        if (solicitud.getId() == 0) {
+            throw new Exception("Al modificar me tienes que pasar el id");
+        }
     }
 
-    @DeleteMapping
-    public void eliminar(long id){
-        throw new UnsupportedOperationException("Pendiente");
+    @DeleteMapping("/{id}")
+    public void borrar(@PathVariable("id") long id) {
+
     }
 }
